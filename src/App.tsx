1a25dc0a8b2ab83cc59e860cdd795b5252dd3ebc
Tio Lim <tio.lim@adriel.ai>
2023-01-28 21:38:00 +0900

    feat: add quill editor example

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+
 import { useDragNDrop } from "@tradelunch/usedragndrop";
+import { Example } from "@tradelunch/usequill";
+
+import "./assets/quill.bubble.css";
+import "./assets/quill.snow.css";
 
 function App() {
     const { onMouseDown, onDragStart, onMouseUp, style, dragRef } =
@@ -8,41 +13,52 @@ function App() {
         });
 
     return (
-        <div
-            style={{
-                // willChange: "transform, top, left",
-                background: "#b6b6e5",
-                height: "100vh",
-                width: "100%",
-            }}
-        >
+        <>
             <div
-                style={style}
-                ref={dragRef}
-                // draggable
-                onDragStart={onDragStart}
-                onMouseDown={onMouseDown}
-                onMouseUp={onMouseUp}
+                style={{
+                    // willChange: "transform, top, left",
+                    background: "#b6b6e5",
+                    height: "50vh",
+                    width: "100%",
+                }}
             >
                 <div
-                    style={{
-                        width: "100px",
-                        height: "100px",
-                        backgroundColor: "grey",
-                        display: "flex",
-                        flex: "1",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        margin: "0px",
-                        padding: "0px",
-                        fontWeight: "bold",
-                        border: "2px solid darkgrey",
-                    }}
+                    style={style}
+                    ref={dragRef}
+                    // draggable
+                    onDragStart={onDragStart}
+                    onMouseDown={onMouseDown}
+                    onMouseUp={onMouseUp}
                 >
-                    <span>Hello Drag!!</span>
+                    <div
+                        style={{
+                            width: "100px",
+                            height: "100px",
+                            backgroundColor: "grey",
+                            display: "flex",
+                            flex: "1",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            margin: "0px",
+                            padding: "0px",
+                            fontWeight: "bold",
+                            border: "2px solid darkgrey",
+                        }}
+                    >
+                        <span>Hello Drag!!</span>
+                    </div>
                 </div>
             </div>
-        </div>
+            <div
+                style={{
+                    // height: "500px",
+                    width: "500px",
+                    display: "block",
+                }}
+            >
+                <Example />
+            </div>
+        </>
     );
 }
 
