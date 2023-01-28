e5cd3ffce7badbe8effe772f3bcc3f13513be37a
Tio Lim <tio.lim@adriel.ai>
2023-01-28 15:26:49 +0900

    feat: add drag n drop example

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useDragNDrop } from "@tradelunch/usedragndrop";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    const { onMouseDown, onDragStart, onMouseUp, style, dragRef } =
+        useDragNDrop({
+            position: "absolute",
+        });
+
+    return (
+        <div
+            style={{
+                // willChange: "transform, top, left",
+                background: "#b6b6e5",
+                height: "100vh",
+                width: "100%",
+            }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            <div
+                style={style}
+                ref={dragRef}
+                // draggable
+                onDragStart={onDragStart}
+                onMouseDown={onMouseDown}
+                onMouseUp={onMouseUp}
+            >
+                <div
+                    style={{
+                        width: "100px",
+                        height: "100px",
+                        backgroundColor: "grey",
+                        display: "flex",
+                        flex: "1",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        margin: "0px",
+                        padding: "0px",
+                        fontWeight: "bold",
+                        border: "2px solid darkgrey",
+                    }}
+                >
+                    <span>Hello Drag!!</span>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
